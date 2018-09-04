6c3f07cf1d46abbd614161bb314a6654e9349607
nireldar <37411022+nireldar@users.noreply.github.com>
2018-09-04 17:10:52 +0300

    Add files via upload

---
+++ landingPage1/jScripts/javascript.js
@@ -0,0 +1,21 @@
+var FirstName=document.getElementById("FirstName").value;
+var LastName=document.getElementById("LastName").value;
+var mail=document.getElementById("mail").value;
+
+
+function myFunction(){
+if(FirstName=="")
+{
+    alert("אנא מלא את הפרטים באופן מלא");
+}
+if(LastName=="")
+{
+    alert("אנא מלא את הפרטים באופן מלא");
+
+}
+if(mail=="")
+{
+    alert("אנא מלא את הפרטים באופן מלא");
+
+}
+}
